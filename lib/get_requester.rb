051dac2bff47d2fd516e77c59d57181498b91107
Jorge Ramirez <joramirez96@gmail.com>
2020-04-10 22:41:06 +0000

    Done.

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -1,1 +1,16 @@
 # Write your code here
+require 'json'
+require 'open-uri'
+
+class GetRequester
+
+  def intitialize
+    
+  end
+
+  def get_response_body
+    get_requester = GetRequester.new(URL)
+  end
+
+
+end
