53d7a9b9b4af6948750e3e4e3c9de6fb442dfe5b
github username <github email address>
2020-04-15 10:20:50 -0400

    Done.

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -1,1 +1,16 @@
-# Write your code here
+class GetRequester
+
+  def initialize(url)
+    @url = url
+  end
+
+  def get_response_body
+    uri = URI.parse(URL)
+    response = Net::HTTP.get_response(uri)
+    response.body
+  end
+
+  def parse_json
+    JSON.parse(get_response_body)
+  end
+end
